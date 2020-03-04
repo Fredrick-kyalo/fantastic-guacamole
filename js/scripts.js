55aaa737d06d18cb7557cae4aba3cb47332dfa06
fredrick-kyalo <kyalomwendwa44@gmail.com>
2020-03-04 11:39:14 +0300

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+        $(".one").click(function() {
+            $("#userequests").toggle();
+        });
+        $(".prog").click(function() {
+            $("#progress").toggle();
+        });
+        $(".three").click(function(){
+        $("#usereviews").toggle();
+        });
+        $(".trend").click(function(){
+            $("#TRENDING").toggle();
+
+    });
+});
